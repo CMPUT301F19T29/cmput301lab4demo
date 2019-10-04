5638377385b8794cc6548d2543ab9ec8e7477f51
Song Hui Zhang <songhui@ualberta.ca>
2019-10-03 19:35:09 -0600

    revert

--- app/src/main/java/com/example/cmput301lab4demo/Shape.java
+++ app/src/main/java/com/example/cmput301lab4demo/Shape.java
@@ -3,11 +3,9 @@ package com.example.cmput301lab4demo;
 public abstract class Shape {
   int x;
   int y;
-  String color;
 
-  public Shape(int x, int y, String color) {
+  public Shape(int x, int y) {
     this.x = x;
     this.y = y;
-    this.color = color;
   }
 }
