b4ada2e264525b5707b39e052015b2b48ed80bdd
Song Hui Zhang <songhui@ualberta.ca>
2019-10-03 19:24:27 -0600

    add color

--- app/src/main/java/com/example/cmput301lab4demo/Shape.java
+++ app/src/main/java/com/example/cmput301lab4demo/Shape.java
@@ -2,11 +2,12 @@ package com.example.cmput301lab4demo;
 
 public abstract class Shape {
   int x;
+  int y;
+  String color;
 
-  public Shape(int x, int y) {
+  public Shape(int x, int y, String color) {
     this.x = x;
     this.y = y;
+    this.color = color;
   }
-
-  int y;
 }
