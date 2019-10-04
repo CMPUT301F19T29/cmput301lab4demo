f56f7c3e2e2182baf3111ad8ed998af6c4e5590e
Jordan <jordan@jordanlos.com>
2019-10-03 19:39:31 -0600

    Update Shape.java

--- app/src/main/java/com/example/cmput301lab4demo/Shape.java
+++ app/src/main/java/com/example/cmput301lab4demo/Shape.java
@@ -3,7 +3,7 @@ package com.example.cmput301lab4demo;
 public abstract class Shape {
   int y;
   int x;
-  String color = "blue";
+  String color = "jordans color";
 
   public Shape(int x, int y) {
     this.x = x;
