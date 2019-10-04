cab5aa80044c876b70710a5551f5d810795f7153
Song Hui Zhang <songhui@ualberta.ca>
2019-10-03 19:36:55 -0600

    add turquoise color

--- app/src/main/java/com/example/cmput301lab4demo/Shape.java
+++ app/src/main/java/com/example/cmput301lab4demo/Shape.java
@@ -3,9 +3,11 @@ package com.example.cmput301lab4demo;
 public abstract class Shape {
   int x;
   int y;
+  String color = "turquoise";
 
-  public Shape(int x, int y) {
+  public Shape(int x, int y, String color) {
     this.x = x;
     this.y = y;
+    this.color = color;
   }
 }
